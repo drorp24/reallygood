8b94c47f5e98bd7b8877315c772782c18f8149ca
Dror <drorp24@gmail.com>
2020-09-20 15:23:27 +0300

    fixing reset() & Square

--- src/components/TicTacToe/Square/index.tsx
+++ src/components/TicTacToe/Square/index.tsx
@@ -4,20 +4,14 @@ import React, { memo } from "react";
 
 import styles from "./styles.module.scss";
 
-// memo will save that component from getting re-rendered with every change in board or toggle of currentPlayer
-//
 // I sometimes assume there could be some more ('...rest') arguments that I need to pass onwards to the child
 // The reason I did it here is that in a real app I would want to highlight the 'winning squares'.
 // To do that I would need to control the styling of the Square component from its parent
 // and '...rest' could help in that.
-export default memo(
-  ({ board, line, row, handleClick, ...rest }): JSX.Element => (
-    <div
-      className={styles.Square}
-      onClick={handleClick({ line, row })}
-      {...rest}
-    >
-      {board[line][row]}
+export default memo(function Square({ value, onClick, ...rest }): JSX.Element {
+  return (
+    <div className={styles.Square} {...{ onClick, ...rest }}>
+      {value}
     </div>
-  )
-);
+  );
+});
--- src/components/TicTacToe/index.tsx
+++ src/components/TicTacToe/index.tsx
@@ -14,107 +14,113 @@ const squares = dimensions ** 2 - 1;
 
 // I chose to control the entire board's state here rather than letting any Sqaure control its own 'X'/'O' state
 // because I need to check here after every move if game is won or over.
-// That could potentially make every Square components rerender with every change on the board,
-// but since Square component is protected with memo, no re-rendering should occur.
-const initialBoard = new Array(dimensions)
-  .fill("")
-  .map(() => new Array(dimensions).fill(""));
 
-// 'resetBoard' is required since the 'board' hook is initialized with 'initialBoard', which is passed as reference.
-// That means initialBoard points to board, hence the need for a different, clean variable for resets.
-const resetBoard = new Array(dimensions)
-  .fill("")
-  .map(() => new Array(dimensions).fill(""));
+const initialState = {
+  emptyBoard: () =>
+    new Array(dimensions).fill("").map(() => new Array(dimensions).fill("")),
+  currentPlayer: "X",
+  moves: 0,
+  gameOver: false,
+  winner: "",
+};
 
-export default memo(
-  (): JSX.Element => {
-    const [board, setBoard] = useState(initialBoard);
-    const [currentPlayer, setCurrentPlayer] = useState("X");
-    const [moves, setMoves] = useState(0);
-    const [gameOver, setGameOver] = useState(false);
-    const [winner, setWinner] = useState("");
+export default memo(function TicTacToe(): JSX.Element {
+  // I could place all these state parts in one object but the code seems clearer when they're split
+  // I'm not destructuring initialState for the same reason
+  const [board, setBoard] = useState(initialState.emptyBoard());
+  const [currentPlayer, setCurrentPlayer] = useState(
+    initialState.currentPlayer
+  );
+  const [moves, setMoves] = useState(initialState.moves);
+  const [gameOver, setGameOver] = useState(initialState.gameOver);
+  const [winner, setWinner] = useState(initialState.winner);
 
-    const togglePlayer = () =>
-      setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  const togglePlayer = () =>
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
 
-    // Since we're checking this following every move, a potential win can only occur in either
-    //   -- the last move's line or row (i.e., no need to check all other lines and rows), or
-    //   -- any of the 2 diagonals (in case line & row happen to be placed on either)
-    const winning = ({ line, row }) =>
-      board[line].every((_, row) => board[line][row] === currentPlayer) ||
+  // Since we're checking this following every move, a potential win can only occur in either
+  //   -- the last move's line or row (i.e., no need to check all other lines and rows), or
+  //   -- any of the 2 diagonals (in case line & row happen to be placed on either)
+  const winning = ({ line, row }) =>
+    board[line].every((_, row) => board[line][row] === currentPlayer) ||
+    board.reduce(
+      (acc, _, line) => acc && board[line][row] === currentPlayer,
+      true
+    ) ||
+    (line === row &&
       board.reduce(
-        (acc, _, line) => acc && board[line][row] === currentPlayer,
+        (acc, _, line) => acc && board[line][line] === currentPlayer,
         true
-      ) ||
-      (line === row &&
-        board.reduce(
-          (acc, _, line) => acc && board[line][line] === currentPlayer,
-          true
-        )) ||
-      (line + row === dimensions - 1 &&
-        board.reduce(
-          (acc, _, line) =>
-            acc && board[line][dimensions - line - 1] === currentPlayer,
-          true
-        ));
+      )) ||
+    (line + row === dimensions - 1 &&
+      board.reduce(
+        (acc, _, line) =>
+          acc && board[line][dimensions - line - 1] === currentPlayer,
+        true
+      ));
 
-    // By referring to the 'board' variable from the closure, handleClick can be passed to Square components
-    // and is guaranteed to get board's most current value
-    const handleClick = ({ line, row }) => () => {
-      if (!gameOver && board[line][row] === "") {
-        board[line][row] = currentPlayer;
-        setBoard(board);
-        setMoves(moves + 1);
-        if (winning({ line, row })) {
-          setWinner(currentPlayer);
-          setGameOver(true);
-        } else if (moves === squares) {
-          setGameOver(true);
-        } else {
-          togglePlayer();
-        }
+  // By referring to the 'board' variable from the closure, handleClick can be passed to Square components
+  // and is guaranteed to get board's most current value
+  const handleClick = ({ line, row }) => () => {
+    if (!gameOver && board[line][row] === "") {
+      board[line][row] = currentPlayer;
+      setBoard(board);
+      setMoves(moves + 1);
+      if (winning({ line, row })) {
+        setWinner(currentPlayer);
+        setGameOver(true);
+      } else if (moves === squares) {
+        setGameOver(true);
+      } else {
+        togglePlayer();
       }
-    };
+    }
+  };
 
-    const reset = () => {
-      setBoard(resetBoard);
-    };
+  const reset = () => {
+    // See comments about splitting state and not destructuring initialState
+    setBoard(initialState.emptyBoard());
+    setCurrentPlayer(initialState.currentPlayer);
+    setMoves(initialState.moves);
+    setGameOver(initialState.gameOver);
+    setWinner(initialState.winner);
+  };
 
-    const Row = ({ item, line }) => (
-      <div className={styles.row}>
-        {item.map((_, row) => (
-          <Square
-            key={`${line}${row}`}
-            {...{ board, line, row, handleClick }}
-          />
-        ))}
-      </div>
-    );
+  const Row = ({ item, line }) => (
+    <div className={styles.row}>
+      {item.map((_, row) => (
+        <Square
+          key={`${line}${row}`}
+          value={board[line][row]}
+          onClick={handleClick({ line, row })}
+        />
+      ))}
+    </div>
+  );
 
-    const Board = () =>
-      board.map((item, line) => {
-        return <Row key={line} {...{ item, line }} />;
-      });
+  const Board = () =>
+    board.map((item, line) => {
+      return <Row key={line} {...{ item, line }} />;
+    });
 
-    return (
-      <div className={styles.TicTacToe}>
-        <div className={styles.Container}>
-          <div className={styles.instructions}>
-            Next player: {gameOver ? "Game Over" : currentPlayer}
-          </div>
-          <div className={styles.instructions}>
-            {/* I know you wanted to write nothing if no one wins */}
-            Winner: {gameOver && !winner ? "Tie!" : winner}
-          </div>
-          <button className={styles.button} type="button" onClick={reset}>
-            Reset
-          </button>
+  return (
+    <div className={styles.TicTacToe}>
+      <div className={styles.Container}>
+        <div className={styles.instructions}>
+          Next player: {gameOver ? "Game Over" : currentPlayer}
+        </div>
+        <div className={styles.instructions}>
+          {/* I know you wanted to write nothing if no one wins */}
+          Winner: {gameOver && !winner ? "Tie!" : winner}
+        </div>
+        <button className={styles.button} type="button" onClick={reset}>
+          Reset
+        </button>
 
-          <div className={styles.Board}>
-            <Board />
-          </div>
+        <div className={styles.Board}>
+          <Board />
         </div>
       </div>
-    );
-  }
-);
+    </div>
+  );
+});
